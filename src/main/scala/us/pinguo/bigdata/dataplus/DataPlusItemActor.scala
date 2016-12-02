607a6196b0cb1f56b3e9b059ba876d6e7eb62251
junheng.gong <gongjunheng@camera360.com>
2016-12-02 15:17:09 +0800

    dont known how to say

--- src/main/scala/us/pinguo/bigdata/dataplus/DataPlusItemActor.scala
+++ src/main/scala/us/pinguo/bigdata/dataplus/DataPlusItemActor.scala
@@ -7,42 +7,54 @@ import us.pinguo.bigdata.DataPlusActor.{ItemTag, TaggingError}
 import us.pinguo.bigdata.PhotoStatisticActor.Report
 import us.pinguo.bigdata.dataplus.DataPlusItemActor._
 import us.pinguo.bigdata.{DataPlusActor, HttpStatusCodeError, PhotoStatisticActor, http}
+import akka.pattern._
+import com.ning.http.client.Response
 
 import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class DataPlusItemActor(signature: DataPlusSignature, organize_code: String) extends DataPlusActor {
+
   import context._
-  val report = context.actorSelection("/user/report")
+
+  private val report = context.actorSelection("/user/report")
+
+  private implicit val formatter = DefaultFormats
+
+  private var processingBody: Array[Byte] = _
+
 
   override def receive: Receive = {
     case RequestItem(body) =>
-      implicit val formatter = DefaultFormats
+      processingBody = body
+
       val requestURL = PATTERN_ITEM_URL.format(organize_code, signature.md5(body))
       val headers = signature.header(requestURL, body, "PUT", "*/*", "*/*")
-      val result = http(requestURL)
+      pipe(http(requestURL)
         .headers(headers.toArray: _*)
-        .putByte(body)
-        .request
+        .putByte(processingBody)
+        .request) to self
 
       report ! Report("dp-item", PhotoStatisticActor.REQUEST)
-      result.map {
-        case Left(e:HttpStatusCodeError) =>
-          parent ! TaggingError(s"DataPlusItemActor-> http error:[${e.getMessage}]")
-          report ! Report("dp-item", e.getCode.toString)
 
-        case Right(response) =>
-          if (response.getStatusCode == SERVER_BUSY) {
-            context.system.scheduler.scheduleOnce(DEFAULT_MILLS millis, self, RequestItem(body))
-            report ! Report("dp-item", PhotoStatisticActor.RETRY)
+    case Left(e: HttpStatusCodeError) =>
+      parent ! TaggingError(s"DataPlusItemActor-> http error:[${e.getMessage}]")
+      report ! Report("dp-item", e.getCode.toString)
+
+    case Right(response: Response) =>
+      response.getStatusCode match {
+
+        case SERVER_BUSY =>
+          context.system.scheduler.scheduleOnce(DEFAULT_MILLS millis, self, RequestItem(processingBody))
+          report ! Report("dp-item", PhotoStatisticActor.RETRY)
+
+        case SUCCESS_CODE =>
+          try parent ! read[ItemTag](response.getResponseBody) catch {
+            case ex: Exception => TaggingError(s"DataPlusItemActor-> parse error:[${ex.getMessage}]")
           }
-          else if (response.getStatusCode == SUCCESS_CODE) {
-            try {
-              parent ! read[ItemTag](response.getResponseBody)
-            } catch {
-              case ex: Exception => TaggingError(s"DataPlusItemActor-> parse error:[${ex.getMessage}]")
-            }
-          }
-          else parent ! TaggingError(s"DataPlusItemActor->code:[${response.getStatusCode}] body[${response.getResponseBody.replaceAll("\n","")}]")
+
+        case _ =>
+          parent ! TaggingError(s"DataPlusItemActor->code:[${response.getStatusCode}] processingBody[${response.getResponseBody.replaceAll("\n", "")}]")
       }
   }
 }
